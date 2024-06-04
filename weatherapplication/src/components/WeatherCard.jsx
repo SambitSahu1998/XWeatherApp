e6f502c30e707eb2cd72e2c0caaf184cedf2d0a7
Sambit Sahu <Sambit.Sahu@yovant.com>
2024-06-04 08:01:44 +0530

    Background Colour and Font Size Improvement

--- weatherapplication/src/components/WeatherCard.jsx
+++ weatherapplication/src/components/WeatherCard.jsx
@@ -4,8 +4,8 @@ import './WeatherCard.css';
 const WeatherCard = ({title,value}) => {
   return (
     <div className="weather-card">
-        <h3>{title}</h3>
-        <p>{value}</p>
+        <h3 style={{fontSize:"20px"}}>{title}</h3>
+        <p style={{fontSize:"16px"}}>{value}</p>
     </div>
   )
 }
