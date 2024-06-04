7807522a195b8b1edfde5e784d003d83309495b9
Sambit Sahu <Sambit.Sahu@yovant.com>
2024-06-04 07:56:32 +0530

    Completed As Requirements

---
+++ weatherapplication/src/components/WeatherApp.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import WeatherCard from "./WeatherCard";
+import './WeatherApp.css';
+
+function WeatherApp() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchWeatherData = async () => {
+    setLoading(true);
+    setWeatherData(null);
+    try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=225e4f16b04a493385295051240306&q=${city}`
+      );
+      if (!response.ok) throw new Error("City not found");
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (err) {
+      alert("Failed to fetch weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <div className="subContainer">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city name"
+        />
+        <button onClick={fetchWeatherData}>Search</button>
+      </div>
+      {loading && <p id="loadingMessage">Loading data…</p>}
+      {weatherData && (
+        <div className="weather-cards">
+          <WeatherCard
+            title="Temperature"
+            value={`${weatherData.current.temp_c}°C`}
+          />
+          <WeatherCard
+            title="Humidity"
+            value={`${weatherData.current.humidity}%`}
+          />
+          <WeatherCard
+            title="Condition"
+            value={weatherData.current.condition.text}
+          />
+          <WeatherCard
+            title="Wind Speed"
+            value={`${weatherData.current.wind_kph} kph`}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default WeatherApp;
---
+++ weatherapplication/src/components/WeatherCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WeatherCard.css';
+
+const WeatherCard = ({title,value}) => {
+  return (
+    <div className="weather-card">
+        <h3>{title}</h3>
+        <p>{value}</p>
+    </div>
+  )
+}
+
+export default WeatherCard;
